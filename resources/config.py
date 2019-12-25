c49f7fb43cd87f02d5e82f3b981b351f3ced9be3
alonha <alonha@gobrowsi.com>
2019-12-25 11:51:34 +0200

    small fix

---
+++ resources/config.py
@@ -0,0 +1,9 @@
+# ALS hyperparams
+d = 11  # latent dim
+iters = 100  # iterations
+early_stop = 10  # number of unimproved iterations to break program
+hyperparams_als={'lu': 88.572, 'li':0.746, 'lbu':8.076, 'lbi':1.48}
+
+
+#SGD hyperparams
+hyperparams_sgd = {'a': 0.238, 'l_u':0.104, 'l_v':0.065, 'l_bm': 0.183, 'l_bn':0.178, 'k':15, 'mean': 0, 'sigma': 0.1}
